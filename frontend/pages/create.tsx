d7958a20f50555549f3e96dccbefc8b0248aa279
0xnuggetz <0xnuggetz@gmail.com>
2023-05-29 19:10:33 -0700

    fix build

--- frontend/pages/create.tsx
+++ frontend/pages/create.tsx
@@ -1,17 +1,7 @@
-import { useRouter } from "next/router";
-import { Web3Storage } from "web3.storage";
 import CreateToken from "@components/CreateToken";
-import CreateCollection from "@components/CreateCollection";
 
 function Create() {
-  const router = useRouter();
-  const { type } = router.query;
-
-  if (type === "token") {
-    return <CreateToken />;
-  } else {
-    return <CreateCollection />;
-  }
+  return <CreateToken />;
 }
 
 export default Create;
